d784a8da2279983c66ef98cc033c59a19fa1128f
Marcin Wąsik <marcinmw99@gmail.com>
2024-05-13 20:16:44 +0200

    feat: implement back pressable to go to homescreen

--- app/(stack)/article/[articleId].tsx
+++ app/(stack)/article/[articleId].tsx
@@ -1,9 +1,20 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { useRouter } from 'expo-router';
 
 export default function Article() {
+  const router = useRouter();
+
   return (
     <SafeAreaView style={styles.root}>
       <View style={styles.container}>
+        <View style={styles.backPressableContainer}>
+          <Pressable
+            onPress={() => router.back()}
+            style={({ pressed }) => [styles.backPressable, pressed && styles.backPressablePressed]}
+          >
+            <Text style={styles.backPressableLabel}>Back</Text>
+          </Pressable>
+        </View>
         <Text>Article</Text>
       </View>
     </SafeAreaView>
@@ -20,5 +31,21 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     flex: 1,
     backgroundColor: '#fff',
+  },
+  backPressableContainer: {
+    display: 'flex',
+    alignItems: 'flex-start',
+  },
+  backPressable: {
+    padding: 12,
+    margin: 8,
+    borderRadius: 12,
+  },
+  backPressablePressed: {
+    backgroundColor: '#ddd',
+  },
+  backPressableLabel: {
+    fontSize: 18,
+    textDecorationLine: 'underline',
   },
 });
